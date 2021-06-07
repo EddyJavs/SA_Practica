b3fcb0be65cf36d894b5a88936cdcc166aea2f73
Eddy Javier <eddjavs@gmail.com>
2021-06-07 01:23:06 -0600

    Se hace fallar una prueba

--- proyectosa/e2e/src/app.e2e-spec.ts
+++ proyectosa/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('proyectosa app is running!');
+    expect(await page.getTitleText()).toEqual('Eddy Sirin - 201503699');
   });
 
   afterEach(async () => {
