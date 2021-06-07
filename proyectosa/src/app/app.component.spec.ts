63af7463339ea55965726cd161ba78dfc92e86a5
Eddy Javier <eddjavs@gmail.com>
2021-06-07 02:32:24 -0600

    Se realizo una prueba fallida

--- proyectosa/src/app/app.component.spec.ts
+++ proyectosa/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'proyectosa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyectosa');
+    expect(app.title).toEqual('titulo malo');
   });
 
   it('should render title', () => {
